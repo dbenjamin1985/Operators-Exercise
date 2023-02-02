351ff6af939719d5477f951efd7078d5221d92ea
David Benjamin <davidbenjamin0685@gmail.com>
2023-02-02 13:24:20 -0700

    Exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,23 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Exercise 1");
+            int x = 17;
+            int y = 4;
+            Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4);
+            Console.WriteLine($"17/4 is 4 remainder 1");    
         }
+
     }
 }
