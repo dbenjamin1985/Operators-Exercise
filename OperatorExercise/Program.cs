a9b5df54b070c6709bcd7552e65c286b87e6a636
David Benjamin <davidbenjamin0685@gmail.com>
2023-02-02 14:34:03 -0700

    Added exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -5,20 +5,26 @@
         static void Main()
         {
             Console.WriteLine("Exercise 1");
-            int x = 17;
-            int y = 4;
-            Console.WriteLine(x + y);
-            Console.WriteLine(x - y);
-            Console.WriteLine(x * y);
-
             int a = 17;
             int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+
+            
             int quotient = a / b;
             int remainder = a % b;
 
-            if (a == 17 && b == 4);
-            Console.WriteLine($"17/4 is 4 remainder 1");    
+            if (a == 17 && b == 4) ;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine("What is the radius of your cirlce?");
+            var radius = double.Parse(Console.ReadLine());
+            var answer = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {answer}");
         }
-
+        public static double AreaOfCircle(double radius)
+        {
+            return (Math.PI * radius * radius);
+        }
     }
 }
